c68a4a1de0d2444a575cb7a5429813fda8108c02
Ullas Das <ullas.das@ibm.com>
2021-12-06 20:28:07 +0530

    changes done

--- src/main/java/com/test/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/test/jenkin/api/SpringJenkinsApplication.java
@@ -19,6 +19,7 @@ public class SpringJenkinsApplication {
 
 	public static void main(String[] args) {
 		logger.info("Application Executed...");
+		logger.info("Application Executed two...");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
